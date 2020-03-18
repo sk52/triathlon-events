45561ebe89b06ace20e2481b561703f8e13102e8
sk52 <samuelking94@gmail.com>
2020-03-18 08:34:12 +0000

    test # add health check route and test

--- packages/server/routes/homeRoutes.js
+++ packages/server/routes/homeRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+router.get('/health', function (req, res) {
+    res.status(200);
+    res.send('OK');
+});
+
 router.get('/', function (req, res) {
     res.send('/ route page');
-})
-
-router.get('/me', function (req, res) {
-    res.send('/me route page');
-})
+});
 
 module.exports = router;
--- packages/server/routes/homeRoutes.test.js
+++ packages/server/routes/homeRoutes.test.js
@@ -1,10 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
 
+describe('Health check endpoint', () => {
+    test('Should respond to /health with 200 status', () => {
+        return request(app).get("/health").then(response =>  {
+            expect(response.text).toBe("OK");
+            expect(response.statusCode).toBe(200);
+        });
+    });
+});
+
 describe('Test the root path', () => {
     test('Should respond to GET method', () => {
         return request(app).get("/").then(response => {
-            expect(response.statusCode).toBe(200)
-        })
+            expect(response.statusCode).toBe(200);
+        });
     });
-})
+});
