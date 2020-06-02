ef6a856c3482dc0e34ed8c77f3e21603fe7bf0ea
sk52 <samuelking94@gmail.com>
2020-06-02 21:11:31 +0100

    feature # add event modal

--- packages/client/src/components/events/Event.js
+++ packages/client/src/components/events/Event.js
@@ -1,11 +1,17 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import React from 'react';
+import React, { useState } from "react";
+import EventModal from './EventModal';
 
 const Event = ({
     name,
     description
 }) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div>
             <Card style={{ width: '18rem' }}>
@@ -15,9 +21,10 @@ const Event = ({
                 <Card.Text>
                     {description.length > 300 ? description.slice(0,300).concat('...') : description}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary" onClick={handleShow}>Go somewhere</Button>
             </Card.Body>
             </Card>
+            <EventModal name={name} description={description} show={show} onHide={handleClose}></EventModal>
         </div>
     )
 }
