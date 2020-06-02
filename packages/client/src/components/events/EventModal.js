46218ed43e9ab6e314267e360c37c5c558ec3650
sk52 <samuelking94@gmail.com>
2020-06-02 21:11:31 +0100

    feature # add event modal

---
+++ packages/client/src/components/events/EventModal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button';
+
+const EventModal = ({
+    name,
+    description,
+    show,
+    onHide,
+}) => {
+    return (
+        <Modal show={show} onHide={onHide}>
+            <Modal.Dialog>
+            <Modal.Header closeButton>
+                <Modal.Title>{name}</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <p>{description}</p>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>Close</Button>
+            </Modal.Footer>
+            </Modal.Dialog>
+        </Modal>
+    )
+}
+
+export default EventModal;
